01298b386fa24b673877f24382d22399ac7902a6
Sebb <sebb@apache.org>
2024-05-25 12:14:13 +0100

    typo

--- pagefind.sh
+++ pagefind.sh
@@ -35,7 +35,7 @@ then
     BINDIR=$(mktemp -d)
     TARGET=${BINDIR}/pagefind.tar.gz
     OS_TYPE=x86_64-unknown-linux-musl
-    time wget --q -O ${TARGET} https://github.com/CloudCannon/pagefind/releases/download/v${PAGEFIND_VERSION}/pagefind-v${PAGEFIND_VERSION}-${OS_TYPE}.tar.gz
+    time wget --quiet -O ${TARGET} https://github.com/CloudCannon/pagefind/releases/download/v${PAGEFIND_VERSION}/pagefind-v${PAGEFIND_VERSION}-${OS_TYPE}.tar.gz
     echo "${PAGEFIND_HASH}  ${TARGET}" > ${TARGET}.sha256
     if shasum -a 256 -c ${TARGET}.sha256
     then
